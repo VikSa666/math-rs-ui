998d3fcf3c04259a5332078c3bd640b5e2c3fd87
VikSa666 <92550917+VikSa666@users.noreply.github.com>
2023-08-28 14:27:15 +0000

    feat(wip): implement first ui for math-rs, still not working...

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,107 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <h1>Welcome!</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <textarea name="matA" id="matA" cols="30" rows="10" v-model="matA" />
+    <textarea name="matB" id="matB" cols="30" rows="10" v-model="matB" />
+    <span>=></span>
+    <textarea
+      name="result"
+      id="result"
+      cols="30"
+      rows="10"
+      v-model="result"
+    ></textarea>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <button @click="performSum">+</button>
+    <button @click="performSub">-</button>
+    <button @click="performMul">*</button>
+  </div>
+  <div>
+    <textarea name="mat" id="mat" cols="30" rows="10" v-model="mat"></textarea>
+    <textarea
+      name="result2"
+      id="result2"
+      cols="30"
+      rows="10"
+      v-model="result2"
+    ></textarea>
+  </div>
+  <div>
+    <button @click="preformGaussReduction">Gauss Reduction</button>
+    <button @click="performGaussJordanDeterminant">
+      Gauss-Jordan Determinant
+    </button>
+    <button @click="performGaussJordanInverse">Gauss-Jordan Inverse</button>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script lang="ts" setup>
+import init, { RMatrixF32 } from "math-rs";
+import { ref } from "vue";
+
+const matA = ref("");
+const matB = ref("");
+const result = ref("Nothing yet...");
+
+const performSum = async () => {
+  await init();
+  result.value = RMatrixF32.checked_sum(
+    RMatrixF32.from_string(matA.value, 1e-6),
+    RMatrixF32.from_string(matB.value, 1e-6)
+  ).to_string();
+};
+
+const performSub = async () => {
+  await init();
+  result.value = RMatrixF32.checked_sub(
+    RMatrixF32.from_string(matA.value, 1e-6),
+    RMatrixF32.from_string(matB.value, 1e-6)
+  ).to_string();
+};
+
+const performMul = async () => {
+  await init();
+  result.value = RMatrixF32.checked_mul(
+    RMatrixF32.from_string(matA.value, 1e-6),
+    RMatrixF32.from_string(matB.value, 1e-6)
+  ).to_string();
+};
+
+const mat = ref("");
+const result2 = ref("Nothing yet...");
+
+const preformGaussReduction = async () => {
+  console.log("preformGaussReduction");
+  await init();
+  console.log("init");
+  result2.value = RMatrixF32.from_string(mat.value, 1e-6)
+    .gaussian_triangulation()
+    .to_string();
+};
+
+const performGaussJordanDeterminant = async () => {
+  await init();
+  result2.value = RMatrixF32.from_string(mat.value, 1e-6)
+    .determinant_using_gauss()
+    .toString();
+};
+
+const performGaussJordanInverse = async () => {
+  await init();
+  result2.value = RMatrixF32.from_string(mat.value, 1e-6)
+    .inverse_gauss_jordan()
+    .to_string();
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(), // This plugin is needed in order to enable compatibility with wasm (not by default)
+    wasm(),
+    // Needed because it must wait `wasm` to be loaded first, therefore it must await. Visit https://www.npmjs.com/package/vite-plugin-wasm
+    topLevelAwait(),
+  ],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
