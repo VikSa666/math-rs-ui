4b1ca643be0c598f0c468dad76fd785d186e4ecd
VikSa666 <92550917+VikSa666@users.noreply.github.com>
2023-09-20 10:21:01 +0000

    refactor: use vite

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import init from "math-rs";
 
-createApp(App).mount("#app");
+init().then(() => createApp(App).mount("#app"));
--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,20 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": {},
+  },
   plugins: [
-    vue(), // This plugin is needed in order to enable compatibility with wasm (not by default)
+    vue(),
+    // This plugin is needed in order to enable compatibility with wasm (not by default)
     wasm(),
     // Needed because it must wait `wasm` to be loaded first, therefore it must await. Visit https://www.npmjs.com/package/vite-plugin-wasm
     topLevelAwait(),
   ],
-  server: {
-    fs: {
-      strict: false,
+  optimizeDeps: {
+    exclude: ["math-rs"],
+    esbuildOptions: {
+      target: "es2020",
     },
   },
 });
